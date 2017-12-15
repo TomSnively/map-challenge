6a8ed3e5b7db8722ee9dd3231719d3e4a72ea519
Tom Snively <voxbop@gmail.com>
2017-12-15 17:51:25 -0500

    Transferring to other computer

--- map-challenge.js
+++ map-challenge.js
@@ -1,1 +1,11 @@
 console.log('we ran');
+
+
+// data is being read from the JSON file
+//data = JSON.parse(data);
+console.log(data);
+
+let continents = [];
+continents = data.continents;
+console.log(continent[0]);
+console.log(continent[1]);
