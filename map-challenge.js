413df8192243ce4132f8b6c710d0d4a0502809b7
Tom Snively <voxbop@gmail.com>
2017-12-16 16:02:08 -0500

    Making progress on recusrion. There is a bug if we have 2 branches, error with city river. Keep a stack? Also need to store this data properly.

--- map-challenge.js
+++ map-challenge.js
@@ -2,6 +2,7 @@
 // data is being read from the JSON file
 //data = JSON.parse(data);
 console.log(dataObject);
+let data = {};
 
 // continents
 // country
@@ -9,7 +10,44 @@ console.log(dataObject);
 // city and river
 // then
 
+// We have a bug when we get to "city" "river"
+// objectType isn't being remembered when we come out of the recursion
 
+function eachRecursive(obj, isKey, objectType) {
+    for (let k in obj) {
+        if (typeof obj[k] === "string" && obj[k] !== null) {
+            console.log('it is string, objectType, k are: ' + objectType, obj[k]);
+            //data[objectType].push(obj[k]);
+
+        } else if (typeof obj[k] === "object" && obj[k] !== null){
+            if (isNaN(k)) {
+                isKey = !isKey;
+                if (isKey) {
+                    //sconsole.log('left side key, k = ' + k);
+                    objectType = k;
+                } else {
+                    console.log('right side object, objectType, k are: ', objectType, k);
+                    //sdata[objectType].push(k);
+                }
+
+            } else {
+                //sconsole.log('k is a number index (ignore), k = ' + k);
+            }
+            //console.log('about to go in again, k is ' + k);
+            eachRecursive(obj[k], isKey, objectType);
+        } else {
+            console.log('error');
+
+        }
+
+    }                       // end of for loop, chance to pop a level?
+    console.log('for loop over, leaving function. Pop our last objectType??');s
+}
+
+let isKey = false;
+eachRecursive(dataObject, isKey, "");
+
+/*
 let data = {};
 let thisLevel = "";
 
@@ -17,7 +55,7 @@ console.log('going through data now');
 for (let x in dataObject) {
     if (typeof x === "string") {
         thisLevel = x;
-        console.log('string', x)
+        console.log('string', x);
         nextLevel = Object.keys(dataObject[x])[0];
 
         // then replace the object with the data
