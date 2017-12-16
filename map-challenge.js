1c02ab7db06009233d6a8003a197288860320213
Tom Snively <voxbop@gmail.com>
2017-12-16 13:35:41 -0500

    Moving to other computer

--- map-challenge.js
+++ map-challenge.js
@@ -1,11 +1,61 @@
-console.log('we ran');
-
 
 // data is being read from the JSON file
 //data = JSON.parse(data);
-console.log(data);
+console.log(dataObject);
 
-let continents = [];
-continents = data.continents;
-console.log(continent[0]);
-console.log(continent[1]);
+// continents
+// country
+// state or
+// city and river
+// then
+
+
+let data = {};
+let thisLevel = "";
+
+console.log('going through data now');
+for (let x in dataObject) {
+    if (typeof x === "string") {
+        thisLevel = x;
+        console.log('string', x)
+        nextLevel = Object.keys(dataObject[x])[0];
+
+        // then replace the object with the data
+//    tempLevel[x] = Object.keys(dataObject[x])[0];
+
+    } else if (typeof x === "object") {
+        console.log('object', x);
+
+    }
+}
+/*
+
+
+continents = data.continent;
+
+function getDataLowerLevel (upperLevel) {
+    let tempLevel = [];
+    for (let x in upperLevel) {
+        if (typeof upperLevel[x] === "object") {
+            // here, grab the inside object, in this case countries
+            countries = getDataLowerLevel(upperLevel[x]);
+
+            // then replace the object with the data
+            tempLevel[x] = Object.keys(upperLevel[x])[0];
+        }
+    }
+
+    console.log(upperLevel);
+    return upperLevel;
+}
+
+
+continents = getDataLowerLevel(continents);
+console.log('at the end, continents', continents);
+
+
+let test = {};
+
+test['countries'] = ['China', 'UK', 'France'];
+test['cities'] = ['Philly', 'Albany', 'Paris'];
+*/
